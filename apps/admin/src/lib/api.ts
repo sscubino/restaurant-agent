120abb8570bc4bcb188fd0125e2cd0ec24b3be77
Santiago Cubino <sscubino@gmail.com>
2025-06-25 21:35:02 +0200

    fix: reload on login page

--- apps/admin/src/lib/api.ts
+++ apps/admin/src/lib/api.ts
@@ -23,7 +23,9 @@ api.interceptors.response.use(
   (error) => {
     if (error?.response?.status === 401) {
       localStorage.removeItem("token");
-      window.location.href = "/login";
+      if (window.location.pathname !== "/login") {
+        window.location.href = "/login";
+      }
     }
     return Promise.reject(error);
   }
